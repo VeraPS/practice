a6b2014bae118053ec8a5e68888773f053945cec
VeraPS <lolla1496@gmail.com>
2017-12-16 10:59:26 +1000

    new db
    stupid

--- db/practice_dvfu.sql
+++ db/practice_dvfu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Дек 10 2017 г., 10:51
+-- Время создания: Дек 16 2017 г., 03:56
 -- Версия сервера: 5.5.53
 -- Версия PHP: 5.5.38
 
@@ -40,9 +40,6 @@ CREATE TABLE `call_status` (
 
 CREATE TABLE `coach` (
   `id_coach` int(4) UNSIGNED NOT NULL,
-  `name_coach` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `tel` varchar(64) DEFAULT NULL,
   `other_conts` varchar(2000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -63,24 +60,25 @@ CREATE TABLE `concern` (
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `groups`
+-- Структура таблицы `groups_dvfu_db`
 --
 
-CREATE TABLE `groups` (
+CREATE TABLE `groups_dvfu_db` (
   `id_group` int(4) UNSIGNED NOT NULL,
   `number` varchar(8) DEFAULT NULL,
-  `title` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `title` varchar(255) DEFAULT NULL,
+  `id_coach` int(4) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 --
--- Дамп данных таблицы `groups`
+-- Дамп данных таблицы `groups_dvfu_db`
 --
 
-INSERT INTO `groups` (`id_group`, `number`, `title`) VALUES
-(1, 'Б8419а', 'Прикладная информатика в дизайне'),
-(2, 'Б8119а', 'Прикладная информатика в дизайне'),
-(3, 'Б8419б', 'Прикладная Информатика в экономике '),
-(4, 'Б8119б', 'Прикладная информатика в экономике');
+INSERT INTO `groups_dvfu_db` (`id_group`, `number`, `title`, `id_coach`) VALUES
+(1, 'Б8419а', 'Прикладная информатика в дизайне', 0),
+(2, 'Б8119а', 'Прикладная информатика в дизайне', 0),
+(3, 'Б8419б', 'Прикладная Информатика в экономике ', 0),
+(4, 'Б8119б', 'Прикладная информатика в экономике', 0);
 
 -- --------------------------------------------------------
 
@@ -90,13 +88,20 @@ INSERT INTO `groups` (`id_group`, `number`, `title`) VALUES
 
 CREATE TABLE `students` (
   `id_student` int(4) UNSIGNED NOT NULL,
-  `name_st` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `tel` varchar(64) DEFAULT NULL,
-  `skills` varchar(2000) DEFAULT NULL,
-  `id_group` int(4) UNSIGNED NOT NULL,
-  `id_coach` int(4) UNSIGNED NOT NULL
+  `title_of_skills` varchar(255) NOT NULL,
+  `skills` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `students_dvfu_db`
+--
+
+CREATE TABLE `students_dvfu_db` (
+  `id_student_dvfu` int(4) UNSIGNED NOT NULL,
+  `id_group` int(4) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- --------------------------------------------------------
 
@@ -113,26 +118,29 @@ CREATE TABLE `terms` (
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `users_dvfu`
+-- Структура таблицы `users_dvfu_db`
 --
 
-CREATE TABLE `users_dvfu` (
+CREATE TABLE `users_dvfu_db` (
   `id_user_dvfu` int(4) UNSIGNED NOT NULL,
   `login` varchar(255) NOT NULL,
   `pass` varchar(255) NOT NULL,
-  `role` int(1) NOT NULL COMMENT '0 -admin;1-students;2-coach;3-concern'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `role` int(1) NOT NULL COMMENT '0 -admin;1-students;2-coach;3-concern',
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `tel` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 --
--- Дамп данных таблицы `users_dvfu`
+-- Дамп данных таблицы `users_dvfu_db`
 --
 
-INSERT INTO `users_dvfu` (`id_user_dvfu`, `login`, `pass`, `role`) VALUES
-(1, 'student1', '1', 1),
-(2, 'student2', '2', 1),
-(3, 'coach1', '1', 2),
-(4, 'admin1', '1', 0),
-(5, 'concern1', '1', 3);
+INSERT INTO `users_dvfu_db` (`id_user_dvfu`, `login`, `pass`, `role`, `name`, `email`, `tel`) VALUES
+(1, 'student1', '1', 1, '', '', ''),
+(2, 'student2', '2', 1, '', '', ''),
+(3, 'coach1', '1', 2, '', '', ''),
+(4, 'admin1', '1', 0, '', '', ''),
+(5, 'concern1', '1', 3, '', '', '');
 
 -- --------------------------------------------------------
 
@@ -173,18 +181,27 @@ ALTER TABLE `concern`
   ADD PRIMARY KEY (`id_conc`);
 
 --
--- Индексы таблицы `groups`
+-- Индексы таблицы `groups_dvfu_db`
 --
-ALTER TABLE `groups`
-  ADD PRIMARY KEY (`id_group`);
+ALTER TABLE `groups_dvfu_db`
+  ADD PRIMARY KEY (`id_group`),
+  ADD KEY `id_coach` (`id_coach`),
+  ADD KEY `id_coach_2` (`id_coach`);
 
 --
 -- Индексы таблицы `students`
 --
 ALTER TABLE `students`
   ADD PRIMARY KEY (`id_student`),
-  ADD KEY `id_group` (`id_group`),
-  ADD KEY `coach` (`id_coach`);
+  ADD KEY `id_student` (`id_student`),
+  ADD KEY `id_student_2` (`id_student`);
+
+--
+-- Индексы таблицы `students_dvfu_db`
+--
+ALTER TABLE `students_dvfu_db`
+  ADD PRIMARY KEY (`id_student_dvfu`),
+  ADD KEY `id_group` (`id_group`);
 
 --
 -- Индексы таблицы `terms`
@@ -193,9 +210,9 @@ ALTER TABLE `terms`
   ADD PRIMARY KEY (`terms_id`);
 
 --
--- Индексы таблицы `users_dvfu`
+-- Индексы таблицы `users_dvfu_db`
 --
-ALTER TABLE `users_dvfu`
+ALTER TABLE `users_dvfu_db`
   ADD PRIMARY KEY (`id_user_dvfu`);
 
 --
@@ -203,8 +220,8 @@ ALTER TABLE `users_dvfu`
 --
 ALTER TABLE `vacancy`
   ADD PRIMARY KEY (`id_vacancy`),
-  ADD KEY `id_conc` (`id_conc`),
-  ADD KEY `term_of_practice` (`term_of_practice`);
+  ADD KEY `term_of_practice` (`term_of_practice`),
+  ADD KEY `id_conc` (`id_conc`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -221,14 +238,14 @@ ALTER TABLE `coach`
 ALTER TABLE `concern`
   MODIFY `id_conc` int(4) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT для таблицы `students`
+-- AUTO_INCREMENT для таблицы `students_dvfu_db`
 --
-ALTER TABLE `students`
-  MODIFY `id_student` int(4) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `students_dvfu_db`
+  MODIFY `id_student_dvfu` int(4) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT для таблицы `users_dvfu`
+-- AUTO_INCREMENT для таблицы `users_dvfu_db`
 --
-ALTER TABLE `users_dvfu`
+ALTER TABLE `users_dvfu_db`
   MODIFY `id_user_dvfu` int(4) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT для таблицы `vacancy`
@@ -250,21 +267,26 @@ ALTER TABLE `call_status`
 -- Ограничения внешнего ключа таблицы `coach`
 --
 ALTER TABLE `coach`
-  ADD CONSTRAINT `coach_ibfk_1` FOREIGN KEY (`id_coach`) REFERENCES `users_dvfu` (`id_user_dvfu`);
+  ADD CONSTRAINT `coach_ibfk_1` FOREIGN KEY (`id_coach`) REFERENCES `users_dvfu_db` (`id_user_dvfu`);
 
 --
 -- Ограничения внешнего ключа таблицы `concern`
 --
 ALTER TABLE `concern`
-  ADD CONSTRAINT `concern_ibfk_1` FOREIGN KEY (`id_conc`) REFERENCES `users_dvfu` (`id_user_dvfu`);
+  ADD CONSTRAINT `concern_ibfk_1` FOREIGN KEY (`id_conc`) REFERENCES `users_dvfu_db` (`id_user_dvfu`);
 
 --
 -- Ограничения внешнего ключа таблицы `students`
 --
 ALTER TABLE `students`
-  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`id_coach`) REFERENCES `coach` (`id_coach`),
-  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`id_group`) REFERENCES `groups` (`id_group`),
-  ADD CONSTRAINT `students_ibfk_3` FOREIGN KEY (`id_student`) REFERENCES `users_dvfu` (`id_user_dvfu`);
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`id_student`) REFERENCES `users_dvfu_db` (`id_user_dvfu`);
+
+--
+-- Ограничения внешнего ключа таблицы `students_dvfu_db`
+--
+ALTER TABLE `students_dvfu_db`
+  ADD CONSTRAINT `students_dvfu_db_ibfk_1` FOREIGN KEY (`id_student_dvfu`) REFERENCES `users_dvfu_db` (`id_user_dvfu`),
+  ADD CONSTRAINT `students_dvfu_db_ibfk_2` FOREIGN KEY (`id_group`) REFERENCES `groups_dvfu_db` (`id_group`);
 
 --
 -- Ограничения внешнего ключа таблицы `terms`
@@ -276,8 +298,7 @@ ALTER TABLE `terms`
 -- Ограничения внешнего ключа таблицы `vacancy`
 --
 ALTER TABLE `vacancy`
-  ADD CONSTRAINT `vacancy_ibfk_1` FOREIGN KEY (`id_conc`) REFERENCES `concern` (`id_conc`),
-  ADD CONSTRAINT `vacancy_ibfk_2` FOREIGN KEY (`term_of_practice`) REFERENCES `terms` (`terms_id`);
+  ADD CONSTRAINT `vacancy_ibfk_1` FOREIGN KEY (`id_conc`) REFERENCES `concern` (`id_conc`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
